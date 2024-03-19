59a4edecdb8bad931af1e5743159882774525623
emiliaharden <emilia.harden@gmail.com>
2024-03-19 15:42:23 +0000

    Added the auth part we did on todays lesson. Not connected to the backend API just yet but working on it.

--- RecipeFE/src/app/app.component.ts
+++ RecipeFE/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { LoginDetails } from './interfaces/login-details';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,30 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'RecipeFE';
+
+  loginDetails: LoginDetails;
+
+  user: User;
+
+  constructor(private auth: AuthService){
+    this.loginDetails = {
+      email:"emi@emi.se",
+      password:"larre123"
+    }
+
+    this.user = {
+      id:-1,
+      name:"emilia",
+      email:"emi@emi.se"
+    }
+
+    auth.loginUser(this.loginDetails);
+  }
+
+  getUser(){
+    this.auth.getUser2().subscribe(res => {
+      console.log(res[0]);
+      this.user = res[0];
+    })
+  }
 }
---
+++ RecipeFE/src/app/services/auth.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginDetails } from '../interfaces/login-details';
+import { Observable, catchError, throwError } from 'rxjs';
+import { User } from '../interfaces/user';
+
+interface ResultData {
+  token: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl = 'http://127.0.0.1:8000/api/';
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  constructor(private http: HttpClient) { }
+
+  loginUser(LoginDetails: LoginDetails) {
+    this.http.post<ResultData>(this.baseUrl + 'login', LoginDetails, this.httpOptions).pipe(
+      catchError(this.handleError)).subscribe(result => {
+        this.httpOptions.headers = this.httpOptions.headers.set('Authorization', "Bearer " + result.token);
+      })
+  }
+
+  //det här kan man nog ta bort sen
+  getUser2(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'getuser/2', this.httpOptions);
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 404) {
+      // A client-side or network error occured. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+}
